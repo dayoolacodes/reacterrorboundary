4e1d711de805d517909b79f3233b445b3fc6784d
dayoola <ayodayo1@gmail.com>
2020-08-18 16:48:58 +0100

    'wip'

--- src/App.js
+++ src/App.js
@@ -18,26 +18,37 @@ class App extends Component {
         id: 3,
         name: "Ayoola"
       }
-
-    ]
+    ],
+    showDivToggle: false
    }
-
-  deleteperson = (id) =>{
-    console.log("clicked")
-    const person = this.state.person.splice(id);
+   
+   deleteperson = (idNum) =>{
+     console.log("clicked", idNum)
+    const person = this.state.person
+    person.splice(idNum, 1);
     this.setState({person: person})
   }
   
+ 
+Toggle=()=>{
+  this.setState({showDivToggle:!(this.state.showDivToggle)})
+}
 
   render() { 
+    let persons = null
+    if (this.state.showDivToggle){
+      persons = 
+      this.state.person.map( (n, index) => (
+        <Persons key={n.id} click={()=>this.deleteperson(index)} name= {n.name}/>
+      ))
+    }
+
+
     return ( 
     <div className="App">
       <h1>Ni Hao</h1>
-
-      {this.state.person.map( n => (
-        <Persons key={n.id} click={this.deleteperson} name= {n.name}/>
-      ))}
-
+      <button onClick={this.Toggle}>Toggle_Me</button>
+      {persons}
     </div>
 
      );
